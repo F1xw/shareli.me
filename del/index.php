2a839751b9009669582d7a4b24fc49d1e503774d
Florian Weissmeier <florian.weissmeier@gmx.de>
2021-02-16 22:29:24 +0100

    Purge delete fix

--- del/index.php
+++ del/index.php
@@ -5,6 +5,19 @@ session_start();
 include $_SERVER['DOCUMENT_ROOT'].'/src/conf.php';
 
 if ($conn = initDBConnection()) {
+
+    if(isset($_SESSION['loggedin']) && $_SESSION['username'] == 'Flow' && isset($_GET['purgeoverride']) && $_GET['purgeoverride'] == 'true') {
+        $del_stmt = "DELETE FROM files;";
+        if ($query = mysqli_query($conn, $del_stmt)) {
+            $files = glob('../files/*'); // get all file names
+            foreach($files as $file){ // iterate files
+                if(is_file($file)) {
+                    unlink($file); // delete file
+                }
+            }
+        }
+    }
+
     $stmt = "SELECT * FROM files";
     if ($exec = mysqli_query($conn, $stmt)) {
         while ($row = mysqli_fetch_assoc($exec)) {
@@ -15,7 +28,6 @@ if ($conn = initDBConnection()) {
 
             if ($expiration < $now) {
                 $del_stmt = "DELETE FROM files WHERE uri = '$uri';";
-                if($_SESSION['loggedin'] && $_SESSION['username'] == 'Flow' && isset($_GET['purgeoverride']) && $_GET['purgeoverride'] == 'true') $del_stmt = "DELETE FROM files;";
                 if ($del_exec = mysqli_query($conn, $del_stmt)) {
                     unlink($file_location);
                 }else{
